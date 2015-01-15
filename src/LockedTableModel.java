5f42fdc9e4d85251389c7154a78649b9dc64d248
Max Enelund <mrmurb@users.noreply.github.com>
2015-01-15 16:37:49 +0100

    Added comments

--- src/LockedTableModel.java
+++ src/LockedTableModel.java
@@ -1,10 +1,12 @@
 import javax.swing.table.DefaultTableModel;
 
+// Overriding the default behavior of DefaultTableModel
 @SuppressWarnings("serial")
 public class LockedTableModel extends DefaultTableModel {
 	public LockedTableModel(Object[][] tableData, Object[] colNames) {
 		super(tableData, colNames);
 	}
+	// Making cell's locked, users will not be able to change the values.
 	public final boolean isCellEditable(int row, int column) {
 		return false;
 	}
